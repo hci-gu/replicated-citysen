1c9ba7b0c0223c36fb21cfa6599d48b733cd994f
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2025-10-27 13:50:03 +0100

    Simplify Vitest plugin configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vitest/config";
-import type { PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 
-const plugins: PluginOption[] = [react() as PluginOption];
-
 export default defineConfig({
-  plugins,
+  plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
